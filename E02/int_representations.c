cf40cb33f179b24b04fcd880892e0d09ff1c6b0e
Stefan Kapferer <stefan@kapferer.ch>
2015-09-24 15:51:03 +0200

    E02: Implemented E.2.1.c) integer representations

---
+++ E02/int_representations.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+main () {
+	// hexadecimal representation of an int
+	int number1 = 0xff;
+
+	// octal representation of an int
+	int number2 = 023;
+
+	printf("%d\n", number1);
+	printf("%d\n", number2);
+}
